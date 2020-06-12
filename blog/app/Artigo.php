275548834fd8e176e041f7cbff5c331bdeb957d2
Marcelo <marceloengecomp@gmail.com>
2020-06-11 22:46:45 -0300

    first commit

---
+++ blog/app/Artigo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Artigo extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['titulo', 'descricao', 'conteudo', 'data'];
+
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ blog/resources/assets/js/components/Painel.vue
@@ -0,0 +1,55 @@
+<template>
+    <div v-bind:class="defineCor">
+        <div class="panel-heading">{{titulo}}</div>
+
+        <div class="panel-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['titulo', 'cor'],
+        computed:{
+            defineCor: function(){
+                return "panel "+ (this.cor || "panel-default");
+            }
+        }
+    }
+</script>
+
+<style media="screen">
+    .blue
+    {
+        border-color: #0d4a96;
+    }
+    .blue > .panel-heading 
+    {
+        color: #f7f7f7;
+        background-color: #0d4a96;
+        border-color: #0d4a96;
+    }
+
+    .orange
+    {
+        border-color: #fd9800;
+    }
+    .orange > .panel-heading 
+    {
+        color: #f7f7f7;
+        background-color: #fd9800;
+        border-color: #fd9800;
+    }
+
+    .red
+    {
+        border-color: #e80d0d;
+    }
+    .red > .panel-heading 
+    {
+        color: #f7f7f7;
+        background-color: #e80d0d;
+        border-color: #e80d0d;
+    }
+</style>
